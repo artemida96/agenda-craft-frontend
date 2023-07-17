b2b00844170d7cc0163766ee38e6308d32c64afd
artemida96 <artemis.maria.chatziroufa@gmail.com>
2023-07-17 19:36:33 +0300

    feat(tasks): test prettier config extension

--- src/features/tasks/TaskView.tsx
+++ src/features/tasks/TaskView.tsx
@@ -5,31 +5,35 @@ import TaskList from "./components/taskList/TaskList";
 import ListIcon from "shared/icons/ListIcon";
 import { format } from "date-fns";
 
-const TaskView = () =>{
+const TaskView = () => {
+  const [isGridTaskViewClicked, setIsGridTaskView] = useState(false);
 
-    const [isGridTaskViewClicked, setIsGridTaskView] = useState(false);
+  const handleGridTaskViewClick = () => {
+    setIsGridTaskView(true);
+  };
+  const handleListTaskViewClick = () => {
+    setIsGridTaskView(false);
+  };
+  return (
+    <div className="ml-4 mt-4">
+      <button className="button mr-2" onClick={handleListTaskViewClick}>
+        <ListIcon
+          width={24}
+          height={24}
+          color={!isGridTaskViewClicked ? "#cc9f13" : ""}
+        />
+      </button>
+      <button className="button " onClick={handleGridTaskViewClick}>
+        <GridIcon
+          width={24}
+          height={24}
+          color={isGridTaskViewClicked ? "#cc9f13" : ""}
+        />
+      </button>
 
-    const handleGridTaskViewClick = () => {
-        setIsGridTaskView(true);
-    };
-    const handleListTaskViewClick = () => {
-        setIsGridTaskView(false);
-    };
-    return <div className="ml-4 mt-4">
-         
-         <button  className="button mr-2" onClick= {handleListTaskViewClick}>
-                <ListIcon width={24} height={24} color= { !isGridTaskViewClicked?"#cc9f13":''}/>
-            </button>
-            <button  className="button " onClick= {handleGridTaskViewClick}>
-                <GridIcon width={24} height={24} color= { isGridTaskViewClicked?"#cc9f13":''}/>
-            </button>
-           
-            <span className="ml-8">
-                {format(new Date(), 'dd/MM/yyyy')}
-            </span>    
-                {isGridTaskViewClicked ? <TaskGrid/> : <TaskList/> }
-
-         
+      <span className="ml-8">{format(new Date(), "dd/MM/yyyy")}</span>
+      {isGridTaskViewClicked ? <TaskGrid /> : <TaskList />}
     </div>
-}
+  );
+};
 export default TaskView;
